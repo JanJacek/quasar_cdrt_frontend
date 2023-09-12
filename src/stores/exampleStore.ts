23eb9778bf9a35861cde6face544a50f0123b952
JanJacek <bobkiewicz.jacek@gmail.com>
2023-09-12 02:42:03 +0200

    Add ydoc, ysocket provider & text binder

--- src/stores/exampleStore.ts
+++ src/stores/exampleStore.ts
@@ -1,12 +1,35 @@
+/* imports */
+// libraries
 import { defineStore } from 'pinia';
 import { io, Socket } from 'socket.io-client';
+import { SocketIOProvider } from 'y-socket.io';
+import * as Y from 'yjs';
+// utils
+// types
+// stores
+// components
+
+/* script*/
 const serverURL = 'http://localhost:3000';
 export const exampleStore = defineStore('counter', {
   state: (): {
+    ydoc: Y.Doc;
     socket: Socket;
+    provider: SocketIOProvider | null;
   } => ({
+    ydoc: new Y.Doc(),
     socket: io(serverURL),
+    provider: null,
   }),
   getters: {},
-  actions: {},
+  actions: {
+    runProvider() {
+      this.provider = new SocketIOProvider(serverURL, '2023', this.ydoc, {
+        autoConnect: true,
+      });
+      // this.provider.on('status', ({ status }: { status: string }) => {
+      //   console.log(status); // Logs "connected" or "disconnected"
+      // });
+    },
+  },
 });
